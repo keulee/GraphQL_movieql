9609ae79cddd9ed6a698bb637970f13aa1361386
Keungeun LEE <keulee42@gmail.com>
2021-01-08 16:52:16 +0100

    Creating first Mutation

--- graphql/db.js
+++ graphql/db.js
@@ -36,4 +36,14 @@ export const deleteMovie = (id) => {
     } else {
         return false;
     }
-}
+};
+
+export const addMovie = (name, score) => {
+    const newMovie = {
+        id: `${movies.length + 1}`,
+        name,
+        score
+    };
+    movies.push(newMovie);
+    return newMovie;
+};
